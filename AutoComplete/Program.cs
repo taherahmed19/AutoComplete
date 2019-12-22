0e3a3c19ae25b3121975ed88a8903c9e026cfdc3
User <User@DESKTOP-ONSQ70Q>
2019-12-22 14:43:05 +0000

    Working autocomplete

--- AutoComplete/Program.cs
+++ AutoComplete/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace AutoComplete
@@ -110,36 +111,7 @@ namespace AutoComplete
             return str;
         }
 
-        public static string DFS2(TrieNode root, List<TrieNode> visitedNodes, List<string> suggestions, string prefix)
-        {
-
-            List<TrieNode> childs = root.Children;
-
-            if (root.IsEndOfWord && !suggestions.Contains(prefix))
-            {
-                Console.WriteLine("Adding " + str);
-                suggestions.Add(prefix);
-            }
-            foreach (var child in childs)
-            {
-                str += child.Value;
-                if (child.IsEndOfWord && !suggestions.Contains(str))
-                {
-                    Console.WriteLine("Adding " + str);
-                    suggestions.Add(str);
-                    str = "";
-                }
-
-                if (child.Children.Count != 0)
-                {
-                    DFS2(child, visitedNodes, suggestions, prefix);
-                }
-                Console.WriteLine("Should be w or h " + root.Value);
-            }
-       
-            return str;
-        }
-
+   
         public static int PrintAutoSuggestions(TrieNode root, string prefix)
         {
             List<TrieNode> visitedNodes = new List<TrieNode>();
@@ -154,17 +126,62 @@ namespace AutoComplete
             else
             {
                 Console.WriteLine("Carry out search from node = " + visitedNodes[visitedNodes.Count - 1].Value);
-                str = prefix;
+                str = "";
                 int initial = visitedNodes.Count - 1;
-                string word = DFS(visitedNodes[0], visitedNodes, suggestions, prefix);
+                string word = DFS2(visitedNodes[visitedNodes.Count - 1], suggestions, prefix);
 
                 foreach (var suggest in suggestions)
                 {
-                    Console.WriteLine(suggest);
                 }
             }
             return 1;
         }
+
+        public static string DFS2(TrieNode root,  List<string> suggestions, string prefix)
+        {
+
+            List<TrieNode> childs = root.Children;
+
+            if (root.IsEndOfWord && !suggestions.Contains(prefix))
+            {
+                suggestions.Add(prefix);
+            }
+            foreach (var child in childs)
+            {
+                Console.WriteLine(child.Value);
+                if (child.IsEndOfWord)
+                {
+                    Console.WriteLine("." + child.Value);
+                    GoToRoot(child);
+                }
+
+                if (child.Children.Count != 0)
+                {
+                    DFS2(child, suggestions, prefix);
+                }
+            }
+
+
+            return str;
+        }
+
+        public static void GoToRoot(TrieNode leaf)
+        {
+            if (leaf.IsEndOfWord) str += leaf.Value;
+            if(leaf.Parent != null)
+            {
+             //   Console.WriteLine("Leaf " + leaf.Value + " parent " + leaf.Parent.Value);
+                str += leaf.Parent.Value;
+                var sb = new StringBuilder(str.Length);
+                for (var i = str.Length - 1; i >= 0; --i)
+                {
+                    sb.Append(str[i]);
+                }
+                Console.WriteLine(sb.ToString());
+                GoToRoot(leaf.Parent);
+            }
+        }
+
 
     }
 }
